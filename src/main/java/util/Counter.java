698fc3c586815b1affd2edb3a866c297227198ed
ThanadolNich <93907437+ThanadolNich@users.noreply.github.com>
2024-01-19 08:29:58 +0000

    Exercise 2 TestFeature2

--- src/main/java/util/Counter.java
+++ src/main/java/util/Counter.java
@@ -1,57 +1,46 @@
 package util;
 
-import java.util.StringTokenizer;
-
-//This is an example (faulty) class to be used for A4 Task1 and Task2
 public class Counter {
-	public int _ctr;
-	public int _multiplier;
 
-	// method increments _ctr
-	public void increment() {
-		_ctr--; // faulty increment
-	}
+    public int _ctr;
 
-	// method decrements _ctr
-	public void decrement() {
-		_ctr--; // faulty decrement
-	}
+    public Counter() {
+        this._ctr = 0;
+    }
 
-	// method resets _ctr
-	public void reset() {
-		_ctr = 0; // faulty reset, _ctr should rather be 0
-	}
+    public void incrementToPrime() {
+        _ctr = getNextPrime(_ctr + 1);
+    }
 
-	// method multiplies _ctr by n
-	public void multiplyBy(int multiplier) {
-		_multiplier = multiplier; // no fault here
-		_ctr = _ctr * _multiplier; // no fault here
-	}
+    public void decrementToPrime() {
+        _ctr = getPreviousPrime(_ctr - 1);
+    }
 
-	// TODO: dev1- method for increment to closest even number
-	public void incrementToEven() {
-		_ctr = -99;
-	}
+    // Utility methods to find the next and previous primes
+    private int getNextPrime(int num) {
+        while (!isPrime(num)) {
+            num++;
+        }
+        return num;
+    }
 
-	// TODO: dev1- method for decrement to closest even number
-	public void decrementToEven() {
-		_ctr = -99;
-	}
+    private int getPreviousPrime(int num) {
+        while (!isPrime(num)) {
+            num--;
+        }
+        return num;
+    }
 
-	// TODO: dev2- method for increment to closest prime number
-	public void incrementToPrime() {
-		_ctr = -99;
-	}
-
-	// TODO: dev2- method for decrement to closest prime number
-	public void decrementToPrime() {
-		_ctr = -99;
-	}
-
-	// TODO: dev3- count the frequency of word in sentence,
-	// refactor source code from dev1 and dev2
-	public void countFrequency(String word, String sentence) {
-		_ctr = -99;
-	}
-
+    // Check if a number is prime
+    private boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/test/java/TestFeature2.java
+++ src/test/java/TestFeature2.java
@@ -2,41 +2,30 @@
  * 
  */
 
-import org.junit.*;
+ import org.junit.Before;
+import org.junit.Test;
 import org.junit.Assert;
 
-/**
- * Test class for Counter class
- * 
- * @author [YourName]
- * @version [Current Date]
- *
- */
 public class TestFeature2 {
 
-	/**
-	 * Test method for {@link util.Counter#increment()}.
-	 */
-	public util.Counter counter = null;
+    private util.Counter counter;
 
-	@Before
-	public void setUp() {
-		counter = new util.Counter();
-	}
+    @Before
+    public void setUp() {
+        counter = new util.Counter();
+    }
 
-	@Test
-	public void testIncrementToPrime(){
-		counter._ctr = 7;
-		counter.incrementToPrime();
-		Assert.assertEquals(11, counter._ctr);
-	}
-	
-	@Test
-	public void testDecrementToPrime(){
-		counter._ctr = 37;
-		counter.decrementToPrime();
-		Assert.assertEquals(31, counter._ctr);
-	}
-	
+    @Test
+    public void testIncrementToPrime() {
+        counter._ctr = 7;
+        counter.incrementToPrime();
+        Assert.assertEquals(11, counter._ctr);
+    }
 
+    @Test
+    public void testDecrementToPrime() {
+        counter._ctr = 37;
+        counter.decrementToPrime();
+        Assert.assertEquals(31, counter._ctr);
+    }
 }
